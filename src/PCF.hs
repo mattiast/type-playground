6da995496f27547b07ba7b19d4e77e1258a85ea0
Matti Astrand <mattiastr@gmail.com>
2023-02-15 21:16:55 -0600

    Use unbound-generics library for bound variables

--- src/PCF.hs
+++ src/PCF.hs
@@ -1,43 +1,64 @@
 {-|
 Type checking for language PCF, described in chapter 19 of PFPL
 -}
+{-# LANGUAGE MultiParamTypeClasses, DeriveGeneric #-}
 module PCF where
 
 import Data.Map (Map)
 import qualified Data.Map as M
+import Unbound.Generics.LocallyNameless
+import Control.Monad.Trans.Maybe (MaybeT (..))
+import GHC.Generics (Generic)
 
-type N = String
+type N = Name Exp
 
 data T = TNat | TArr T T
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Generic)
 
 type Context = Map N T
+
+eo :: Context
+eo = M.empty
 
 data Exp
   = EVar N
   | EIntZ
   | EIntS Exp
-  | EIfZ Exp (N, Exp) Exp
-  | EAbs N T Exp
+  | EIfZ Exp (Bind N Exp) Exp
+  | EAbs T (Bind N Exp)
   | EApp Exp Exp
-  | EFix T N Exp
-  deriving (Show)
+  | EFix T (Bind N Exp)
+  deriving (Show, Generic)
+
+instance Alpha T
+instance Alpha Exp
+
+instance Subst Exp T
+instance Subst Exp Exp where
+  isvar (EVar x) = Just (SubstName x)
+  isvar _ = Nothing
 
 -- Statics
 
-tc :: Context -> Exp -> Maybe T
-tc g (EVar x) = M.lookup x g
+type MM a = MaybeT FreshM a
+runMM :: MM a -> Maybe a
+runMM = runFreshM . runMaybeT
+
+tc :: Context -> Exp -> MM T
+tc g (EVar x) = MaybeT $ pure $ M.lookup x g
 tc _ EIntZ = pure TNat
 tc g (EIntS e) = do
   TNat <- tc g e
   return TNat
-tc g (EIfZ e0 (x, e1) e) = do
+tc g (EIfZ e0 bnd1 e) = do
+  (x,e1) <- unbind bnd1
   TNat <- tc g e
   t0 <- tc g e0
   t1 <- tc (M.insert x TNat g) e1
   True <- return (t0 == t1)
   return t0
-tc g (EAbs x t1 e) = do
+tc g (EAbs t1 bnd) = do
+  (x,e) <- unbind bnd
   t2 <- tc (M.insert x t1 g) e
   return (TArr t1 t2)
 tc g (EApp e1 e2) = do
@@ -45,7 +66,8 @@ tc g (EApp e1 e2) = do
   TArr t2' t1 <- tc g e1
   True <- return (t2 == t2')
   return t1
-tc g (EFix t x e) = do
+tc g (EFix t bnd) = do
+  (x,e) <- unbind bnd
   t' <- tc (M.insert x t g) e
   True <- return (t == t')
   return t
@@ -53,9 +75,16 @@ tc g (EFix t x e) = do
 -- Examples
 
 plus :: Exp
-plus = EFix (TArr TNat (TArr TNat TNat)) "f"
-    (EAbs "x" TNat (EAbs "y" TNat
-        (EIfZ (EVar "x") ("y", EIntS (EApp (EApp (EVar "f") (EVar "x")) (EVar "y"))) (EVar "y"))))
+plus = EFix (TArr TNat (TArr TNat TNat)) $ bind f (
+  EAbs TNat $ bind x (
+    EAbs TNat $ bind y (
+      EIfZ (EVar x) (bind y $ EIntS (EApp (EApp (EVar f) (EVar x)) (EVar y))) (EVar y)
+    )
+  ))
+  where
+    f = s2n "f"
+    x = s2n "x"
+    y = s2n "y"
 
 z :: Exp
 z = EIntZ
@@ -75,30 +104,28 @@ val e = case e of
   EAbs {} -> True
   _ -> False
 
-subst :: N -> Exp -> Exp -> Exp
-subst x e1 e = case e of
-  EVar y -> if x == y then e1 else e
-  EIntZ -> e
-  EIntS ee -> EIntS (subst x e1 ee)
-  EIfZ e0 (y, ee1) ee -> EIfZ (subst x e1 e0) (y, if x == y then ee1 else subst x e1 ee1) (subst x e1 ee)
-  EAbs y t ee -> if x == y then e else EAbs y t (subst x e1 ee)
-  EApp ee1 ee2 -> EApp (subst x e1 ee1) (subst x e1 ee2)
-  EFix t y ee -> if x == y then e else EFix t y (subst x e1 ee)
-
-step :: Exp -> Maybe Exp
-step (EIfZ e0 _ EIntZ) = Just e0
-step (EIfZ _ (x, e1) (EIntS e)) = Just (subst x e e1)
+step :: Exp -> MM Exp
+step (EIfZ e0 _ EIntZ) = pure e0
+step (EIfZ _ bnd1 (EIntS e)) = do
+  (x, e1) <- unbind bnd1
+  pure (subst x e e1)
 step (EIfZ e0 st e) = do
   e' <- step e
   return (EIfZ e0 st e')
-step (EApp (EAbs x _ e) e2) = Just (subst x e2 e)
+step (EApp (EAbs _ bnd) e2) = do
+  (x, e) <- unbind bnd
+  pure (subst x e2 e)
 step (EApp e1 e2) = do
   e1' <- step e1
   return (EApp e1' e2)
-step e@(EFix _ x e1) = Just (subst x e e1)
-step _ = Nothing
+step e@(EFix _ bnd) = do
+  (x,e1) <- unbind bnd
+  pure (subst x e e1)
+step _ = fail "hjoo"
 
-eval :: Exp -> Exp
-eval e = case step e of
-  Nothing -> e
-  Just e' -> eval e'
+eval :: Exp -> FreshM Exp
+eval e = do
+  e' <- runMaybeT (step e)
+  case e' of
+    Nothing -> return e
+    Just e'' -> eval e''
