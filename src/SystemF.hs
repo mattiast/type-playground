54ce05e91c5d60855e462b34f0bb997771b0e45d
Matti Astrand <mattiastr@gmail.com>
2023-02-15 22:11:06 -0600

    Use unbound-generics for both type and term variables in SystemF

--- src/SystemF.hs
+++ src/SystemF.hs
@@ -1,31 +1,36 @@
-module SystemF (TV, N, Typ (..), Exp (..), TContext, Context, checkType, checkExpr) where
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
+module SystemF (TV, N, Typ (..), Exp (..), TContext, Context, checkType, checkExpr, runMM, MM) where
+
+import Control.Monad.Trans.Except (ExceptT (ExceptT), runExceptT)
 import Data.Map qualified as M
 import Data.Set qualified as S
+import GHC.Generics (Generic)
+import Unbound.Generics.LocallyNameless
 
-type TV = String
+type TV = Name Typ
 
 data Typ
   = TVar TV
   | TArr Typ Typ
-  | TAll TV Typ
-  deriving (Eq)
+  | TAll (Bind TV Typ)
+  deriving (Generic)
 
 instance Show Typ where
-  show (TVar t) = t
+  show (TVar t) = show t
   show (TArr t1 t2) = "(" ++ show t1 ++ ") → " ++ show t2
-  show (TAll t tau) = "∀(" ++ t ++"." ++ show tau ++ ")"
+  show (TAll bnd) = "∀( " ++ show bnd ++ " . )"
 
-
-type N = String
+type N = Name Exp
 
 data Exp
   = EVar N
-  | EAbs Typ N Exp
+  | EAbs Typ (Bind N Exp)
   | EApp Exp Exp
-  | ETAbs TV Exp
+  | ETAbs (Bind TV Exp)
   | ETApp Typ Exp
-  deriving (Show)
+  deriving (Show, Generic)
 
 -- Δ = set of statements "τ is a type"
 type TContext = S.Set TV
@@ -33,21 +38,43 @@ type TContext = S.Set TV
 -- Γ = set of statements "e : τ"
 type Context = M.Map N Typ
 
-checkType :: TContext -> Typ -> Either String ()
-checkType delta (TVar t) = if S.member t delta then return () else Left ("Type variable not in scope: " ++ t)
+instance Subst Typ Typ where
+  isvar (TVar t) = Just (SubstName t)
+  isvar _ = Nothing
+
+instance Alpha Exp
+
+instance Alpha Typ
+
+type MM a = (ExceptT String) FreshM a
+
+runMM :: MM a -> Either String a
+runMM = runFreshM . runExceptT
+
+checkType :: TContext -> Typ -> MM ()
+checkType delta (TVar t) =
+  if S.member t delta
+    then return ()
+    else fale ("Type variable not in scope: " ++ show t)
 checkType delta (TArr t1 t2) = do
   checkType delta t1
   checkType delta t2
-checkType delta (TAll t tau) = checkType (S.insert t delta) tau
+checkType delta (TAll bnd) = do
+  (t, tau) <- unbind bnd
+  checkType (S.insert t delta) tau
 
-grd :: Bool -> String -> Either String ()
-grd b errmsg = if b then Right () else Left errmsg
+fale :: String -> MM a
+fale s = ExceptT $ pure $ Left s
 
-checkExpr :: TContext -> Context -> Exp -> Either String Typ
+grd :: Bool -> String -> MM ()
+grd b errmsg = if b then pure () else fale errmsg
+
+checkExpr :: TContext -> Context -> Exp -> MM Typ
 checkExpr _ gamma (EVar x) = case M.lookup x gamma of
   Just t -> return t
-  Nothing -> Left ("Variable not in scope: " ++ x)
-checkExpr delta gamma (EAbs t1 x e) = do
+  Nothing -> fale ("Variable not in scope: " ++ show x)
+checkExpr delta gamma (EAbs t1 bnd) = do
+  (x, e) <- unbind bnd
   checkType delta t1
   t2 <- checkExpr delta (M.insert x t1 gamma) e
   return (TArr t1 t2)
@@ -56,27 +83,36 @@ checkExpr delta gamma (EApp e1 e2) = do
   case tarr of
     TArr t2' t -> do
       t2 <- checkExpr delta gamma e2
-      grd (t2 == t2') "Application types don't match"
+      grd (aeq t2 t2') "Application types don't match"
       return t
-    _ -> Left "Application of non-function type"
-checkExpr delta gamma (ETAbs t e) = do
+    _ -> fale "Application of non-function type"
+checkExpr delta gamma (ETAbs bnd) = do
+  (t, e) <- unbind bnd
   tau <- checkExpr (S.insert t delta) gamma e
-  return (TAll t tau)
+  return (TAll $ bind t tau)
 checkExpr delta gamma (ETApp tau e) = do
   checkType delta tau
   tall <- checkExpr delta gamma e
   case tall of
-    TAll t tau' -> do
-      return (substType t tau tau')
-    _ -> Left "Type application to non-generic type"
-
-substType :: TV -> Typ -> Typ -> Typ
-substType t tau' (TVar t') = if t == t' then tau' else TVar t'
-substType t tau' (TArr t1 t2) = TArr (substType t tau' t1) (substType t tau' t2)
-substType t tau' (TAll tt tau) = if t == tt then TAll tt tau else TAll tt (substType t tau' tau)
+    TAll bnd -> do
+      (t, tau') <- unbind bnd
+      return (subst t tau tau')
+    _ -> fale "Type application to non-generic type"
 
 unit :: Exp
-unit = ETAbs "r" (EAbs (TVar "r") "x" (EVar "x"))
+unit = ETAbs $ bind r (EAbs (TVar r) $ bind x (EVar x))
+  where
+    r = s2n "r"
+    x = s2n "x"
 
 nat :: Typ
-nat = TAll "t" (TArr (TVar "t") (TArr (TArr (TVar "t") (TVar "t")) (TVar "t")))
+nat = TAll $ bind t (TArr (TVar t) (TArr (TArr (TVar t) (TVar t)) (TVar t)))
+  where
+    t = s2n "t"
+
+zero :: Exp
+zero =
+  let z = s2n "z"
+      s = s2n "s"
+      t = s2n "t"
+   in ETAbs $ bind t $ EAbs (TVar t) $ bind z $ EAbs (TArr (TVar t) (TVar t)) $ bind s $ EVar z
