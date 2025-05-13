3b2bb0c98c26c178d58968d05640ade7ad2dfa56
Matti Astrand <mattiastr@gmail.com>
2025-05-12 22:21:38 -0400

    More test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
-import qualified Infer as I
-import Test.QuickCheck (quickCheckResult, Result (Success))
+
+import Infer qualified as I
+import Test.QuickCheck (Result (Success), quickCheckResult)
+import Control.Monad (forM_)
+import Data.ByteString (ByteString)
 
 main :: IO ()
 main = do
-    Right e <- return $ I.myParse "fun f -> let x = fun g y -> let _ = g(y) in eq(f, g) in x"
+  forM_ test_cases $ \(input, expected) -> do
+    Right e <- return $ I.myParse input
     Just t <- return $ I.infer I.myEnv e
-    let c1 = I.renderScheme (I.generalizeWithABC t) == "forall[a b] (a -> b) -> (a -> b, a) -> bool"
+    let c1 = I.renderScheme (I.generalizeWithABC t) == expected
     Success {} <- quickCheckResult c1
-    putStrLn "Test suite done"
+    pure ()
+  putStrLn "Test suite done"
 
-
+test_cases :: [(ByteString, String)]
+test_cases =
+  [ ("fun x -> let y = fun z -> z in y(y)", "forall[a b] a -> b -> b"),
+    ("fun f -> let x = fun g y -> let _ = g(y) in eq(f, g) in x", "forall[a b] (a -> b) -> (a -> b, a) -> bool")
+  ]
